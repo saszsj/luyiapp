9c7cbeb525883e27a48f7a6244059d0a470e2da5
Kryss <zhan.shengjie@gmail.com>
2019-10-17 14:53:10 +0200

    init_2

--- karma.conf.js
+++ karma.conf.js
@@ -1,33 +1,78 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+'use strict';
 
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+/**
+ * Module dependencies.
+ */
+var _ = require('lodash'),
+  defaultAssets = require('./config/assets/default'),
+  testAssets = require('./config/assets/test'),
+  testConfig = require('./config/env/test'),
+  karmaReporters = ['mocha'];
+
+// Karma configuration
+module.exports = function (karmaConfig) {
+  var configuration = {
+    frameworks: ['jasmine'],
+
+    preprocessors: {
+      'modules/*/client/views/**/*.html': ['ng-html2js']
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
+
+    ngHtml2JsPreprocessor: {
+      moduleName: 'mean',
+
+      cacheIdFromPath: function (filepath) {
+        return filepath;
+      }
     },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: ['progress', 'kjhtml'],
+
+    // List of files / patterns to load in the browser
+    files: _.union(defaultAssets.client.lib.js, defaultAssets.client.lib.tests, defaultAssets.client.js, testAssets.tests.client, defaultAssets.client.views),
+
+    // Test results reporter to use
+    // Possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: karmaReporters,
+
+    // Web server port
     port: 9876,
+
+    // Enable / disable colors in the output (reporters and logs)
     colors: true,
-    logLevel: config.LOG_INFO,
+
+    // Level of logging
+    // Possible values: karmaConfig.LOG_DISABLE || karmaConfig.LOG_ERROR || karmaConfig.LOG_WARN || karmaConfig.LOG_INFO || karmaConfig.LOG_DEBUG
+    logLevel: karmaConfig.LOG_INFO,
+
+    // Enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
     browsers: ['Chrome'],
-    singleRun: false
-  });
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+    // Continuous Integration mode
+    // If true, it capture browsers, run tests and exit
+    singleRun: true
+  };
+
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+
+  karmaConfig.set(configuration);
 };
